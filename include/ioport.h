0198eb5c1dc049b4899cd346ca037cab59a62d3e
jcassette <jcassett@etud.insa-toulouse.fr>
2014-11-01 02:41:19 +0100

    ioport.h: ajout de parentheses dans les macros

--- include/ioport.h
+++ include/ioport.h
@@ -22,21 +22,21 @@
 #define PIN_7   (1 << 7)
 #define PIN_ALL (PIN_0|PIN_1|PIN_2|PIN_3|PIN_4|PIN_5|PIN_6|PIN_7)
 
-#define PortA_SetDirectionIn(pins)      TRISA |= pins
-#define PortA_SetDirectionOut(pins)     TRISA &= ~pins
-#define PortA_SetModeAnalog(pins)       ANCON0 |= pins
-#define PortA_SetModeDigital(pins)      ANCON0 &= ~pins
+#define PortA_SetDirectionIn(pins)      TRISA |= (pins)
+#define PortA_SetDirectionOut(pins)     TRISA &= ~ (pins)
+#define PortA_SetModeAnalog(pins)       ANCON0 |= (pins)
+#define PortA_SetModeDigital(pins)      ANCON0 &= ~ (pins)
 
-#define PortB_SetDirectionIn(pins)      TRISB |= pins
-#define PortB_SetDirectionOut(pins)     TRISB &= ~pins
-#define PortB_SetModeAnalog(pins)       ANCON1 |= pins
-#define PortB_SetModeDigital(pins)      ANCON1 &= ~pins
-#define PortB_EnablePullUp(pins)        WPUB |= pins
-#define PortB_DisablePullUp(pins)       WPUB &= ~pins
+#define PortB_SetDirectionIn(pins)      TRISB |= (pins)
+#define PortB_SetDirectionOut(pins)     TRISB &= ~ (pins)
+#define PortB_SetModeAnalog(pins)       ANCON1 |= (pins)
+#define PortB_SetModeDigital(pins)      ANCON1 &= ~ (pins)
+#define PortB_EnablePullUp(pins)        WPUB |= (pins)
+#define PortB_DisablePullUp(pins)       WPUB &= ~ (pins)
 
-#define PortC_SetDirectionIn(pins)      TRISC |= pins
-#define PortC_SetDirectionOut(pins)     TRISC &= ~pins
-#define PortC_EnableOpenDrain(pins)     ODCON |= pins
-#define PortC_DisableOpenDrain(pins)    ODCON &= ~pins
+#define PortC_SetDirectionIn(pins)      TRISC |= (pins)
+#define PortC_SetDirectionOut(pins)     TRISC &= ~ (pins)
+#define PortC_EnableOpenDrain(pins)     ODCON |= (pins)
+#define PortC_DisableOpenDrain(pins)    ODCON &= ~ (pins)
 
 #endif	/* IOPORT_H */
