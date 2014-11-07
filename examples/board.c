e59386f925dc2cc21053bc0fcce82b79d6ed33fb
jcassette <jcassett@etud.insa-toulouse.fr>
2014-11-07 12:43:54 +0100

    Modif des fichiers d'exemple pour la carte de dev

--- examples/board.c
+++ examples/board.c
@@ -1,31 +1,22 @@
-#if defined(__XC)
-#include <xc.h>             /* XC8 General Include File */
-#include <stdint.h>         /* For uint8_t definition */
-#include <stdbool.h>        /* For true/false definition */
-#endif
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #include <ioport.h>
 
-#pragma config FOSC	= INTIO2
-#pragma config SOSCSEL	= DIG
-#pragma config MCLRE	= OFF
-#pragma config XINST    = OFF
-#pragma config BOREN    = OFF
-#pragma config WDTEN    = OFF
-
-void BoardInit(void)
+void BoardInit()
 {
     /* DIP switch */
     PortA_SetDirectionIn(PIN_0|PIN_1|PIN_2|PIN_3);
-    /* potentiometer */
-    PortA_SetDirectionIn(PIN_5);
-    PortA_SetModeAnalog(PIN_5);     /* RA5 is AN4 */
+    /* Potentiometre */
+    PortA_SetDirectionIn(PIN_5);    /* RA5 = AN4 */
+    PortA_SetModeAnalog(PIN_5);
     /* CAN */
-    PortB_SetDirectionOut(PIN_2);   /* RB2 is CANTX */
-    PortB_SetDirectionIn(PIN_3);    /* RB3 is CANRX */
-    /* LEDs */
+    PortB_SetDirectionOut(PIN_2);   /* RB2 = CANTX */
+    PortB_SetDirectionIn(PIN_3);    /* RB3 = CANRX */
+    /* LED */
     PortC_SetDirectionOut(PIN_0|PIN_1|PIN_2|PIN_3);
     /* UART */
-    PortC_SetDirectionOut(PIN_6);   /* RC6 is TX */
-    PortC_SetDirectionIn(PIN_7);    /* RC7 is RX */
+    PortC_SetDirectionOut(PIN_6);   /* RC6 = TX */
+    PortC_SetDirectionIn(PIN_7);    /* RC7 = RX */
 }
--- examples/board.h
+++ examples/board.h
@@ -1,7 +1,7 @@
 #ifndef __BOARD_H
 #define	__BOARD_H
 
-void BoardInit(void);
+void BoardInit();
 
 #endif	/* __BOARD_H */
 
